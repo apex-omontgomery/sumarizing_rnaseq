0d8898ed2c69d8108ba6b4ad56d86d252a944327
wimo7083 <wimo7083@colorado.edu>
2017-05-23 18:18:51 -0600

    p value heatmap

--- web_app/app.py
+++ web_app/app.py
@@ -18,24 +18,60 @@ from bokeh.plotting import figure
 from flask import Flask, render_template
 app = Flask(__name__)
 
-def get_dataframe_and_axes(fname=None, gene_col_name=None):
+def get_dataframe_and_axes(fname=None,fname2=None, gene_col_name=None, max_value = None):
     ''' arbitrary data for now '''
-    df = pd.read_csv(fname)
-    #df.rename(columns=lambda x: x.strip().replace("'",""))
-    df['Gene_ID'] = df[gene_col_name].astype(str) 
-    df.drop([gene_col_name], axis=1, inplace=True)
-    df = df.set_index('Gene_ID')
-    df.columns.name = 'Samples' 
-    gene_lst = list(df.index)
-    sample_lst = list(df.columns)
-    df2 = pd.DataFrame(df.stack(), columns=['counts']).reset_index()
-    return df2, sample_lst, gene_lst 
+    #count file
+    df_c = pd.read_csv(fname)
+    #p_value file
+    df_p = pd.read_csv(fname2)
+    len_p_val = len(list(df_p)) 
+    len_c_val = len(list(df_c))
+    # renames the A1 position to gene_col_name if not found. not very smart. 
+    if list(df_c)[0] != gene_col_name:
+        df_c.rename(columns = {list(df_c)[0]: gene_col_name}, inplace = True)
+    if list(df_p)[0] != gene_col_name:
+        df_p.rename(columns = {list(df_p)[0]: gene_col_name}, inplace = True)
+    #drop rows with values greater than max p_value
+    # list(df_p)[-1] is the p_value adjusted based off our r script. 
+    df_p = df_p[df_p[list(df_p)[-1]] <= max_value]   
+    #merge on p_value dataframe
+    df_c = pd.merge(df_c, df_p, on='Gene ID')    
+    
+    #keep only columsn from original count dataframe
+    df_c = df_c[df_c.columns[:len_c_val]]
+    
+    
+    
+    df_c['Gene_ID'] = df_c[gene_col_name].astype(str)
+    df_c.drop([gene_col_name], axis=1, inplace=True)
+    df_c = df_c.set_index('Gene_ID')
+    
+    df_p['Gene_ID'] = df_p[gene_col_name].astype(str)
+    df_p.drop([gene_col_name], axis=1, inplace=True)
+    df_p = df_p.set_index('Gene_ID')
+    
+    
+    df_c.columns.name = 'Samples' 
+    gene_lst = list(df_c.index)
+    sample_lst = list(df_c.columns)
+    df_c = pd.DataFrame(df_c.stack(), columns=['counts']).reset_index()
+    df_p =df_p.reset_index()
+    # only need the id and the p_adj for now. 
+    #df_p= df_p[df_p.columns[[0,-1]]]
+        
+    return df_c, sample_lst, gene_lst, df_p
 
-def make_heatmap_object(df, sample_lst, gene_lst):
+ 
+    
+def make_heatmap_object(df, sample_lst, gene_lst, df_p):
     ''' makes a bokeh figure '''
     colors = ["#75968f", "#a5bab7", "#c9d9d3", "#e2e2e2", "#dfccce", "#ddb7b1", "#cc7878", "#933b41", "#550b1d"]
+    colors2 =['#E5B000', '#CE9514', '#B77F24', '#A06C30','#895D36', '#724F39', '#5B4236', '#443430', '#2D2624','#161414', '#000000']
+    colors = colors2[::-1]
     mapper = LinearColorMapper(palette=colors, low=df['counts'].min(), high=df['counts'].max())
 
+    df = pd.merge(df, df_p, on = 'Gene_ID')
+    
     source = ColumnDataSource(df)
 
     TOOLS = "hover,save,pan,box_zoom,reset,wheel_zoom"
@@ -45,11 +81,11 @@ def make_heatmap_object(df, sample_lst, gene_lst):
                x_range=sample_lst, y_range=gene_lst,
                x_axis_location="above", plot_width=600, plot_height=900,
                tools=TOOLS, toolbar_location='below')
-
+    
     p.grid.grid_line_color = None
     p.axis.axis_line_color = None
     p.axis.major_tick_line_color = None
-    p.axis.major_label_text_font_size = "5pt"
+    p.axis.major_label_text_font_size = "10pt"
     p.axis.major_label_standoff = 0
     p.xaxis.major_label_orientation = pi / 3
 
@@ -65,8 +101,9 @@ def make_heatmap_object(df, sample_lst, gene_lst):
     p.add_layout(color_bar, 'right')
 
     p.select_one(HoverTool).tooltips = [
-         ('coord', '@Samples @Gene_ID'),
-         ('count', '@counts'),
+         ('[Sample, Gene_ID]', '@Samples @Gene_ID'),
+         ('Transformed Count', '@counts'),
+         ('P-Adj', '@padj'),
     ]
     return p
 
@@ -103,7 +140,12 @@ def contact():
     return render_template('contact.html')
 
 if __name__ == '__main__':
-    filename = 'data/Example_file.csv'
-    df, sample_lst, gene_lst = get_dataframe_and_axes(filename, 'Gene ID')
-    heatmap = make_heatmap_object(df, sample_lst, gene_lst)
+    filename_count = 'data/d_c.csv'
+    #filename_count = 'data/Example_file.csv'
+    filename_pvalue = 'data/d_p.csv'
+    # rename A1 to Gene_ID?
+    highest_p = .03
+    
+    df, sample_lst, gene_lst, df2 = get_dataframe_and_axes(filename_count, filename_pvalue, 'Gene ID', highest_p)
+    heatmap = make_heatmap_object(df, sample_lst, gene_lst,df2)
     app.run()
